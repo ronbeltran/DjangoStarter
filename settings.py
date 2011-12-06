b00794d65c5d62ac5b31d89bfe977ae62badff4f
Ronnie Beltran <rbbeltran.09@gmail.com>
2011-12-06 21:27:08 +0800

    add local_settings.py for local development

--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database_name_here',                      # Or path to database file if using sqlite3.
+        'USER': 'username_here',                      # Not used with sqlite3.
+        'PASSWORD': 'password_here',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -143,3 +143,10 @@ LOGGING = {
         },
     }
 }
+
+# load development settings
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
